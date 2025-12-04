a025f956d0535d421629971ba803e403599e4ac8
Austin <austin.a.guevara@gmail.com>
2025-12-04 11:07:24 -0800

    broke out code into functions for better testing

--- Day_04/main.zig
+++ Day_04/main.zig
@@ -2,6 +2,17 @@ const std = @import("std");
 
 const Coord = struct { x: usize, y: usize };
 
+const DIRECTIONS = [8][2]i32{
+    .{ -1, 0 }, // left
+    .{ 1, 0 }, // right
+    .{ 0, -1 }, // up
+    .{ 0, 1 }, // down
+    .{ -1, -1 }, // top-left
+    .{ 1, -1 }, // top-right
+    .{ -1, 1 }, // bottom-left
+    .{ 1, 1 }, // bottom-right
+};
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
 
@@ -17,48 +28,41 @@ pub fn main() !void {
 }
 
 pub fn part1(allocator: std.mem.Allocator, input: []const u8) !usize {
-    //TODO: Implement solution
-    var valid_rolls: usize = 0;
     var map = std.ArrayHashMap(Coord, u8, std.array_hash_map.AutoContext(Coord), true).init(allocator);
     defer map.deinit();
 
-    var current_pos: Coord = .{ .x = 0, .y = 0 };
+    const max_size = try mapBuilder(&map, input);
+
+    return removeRoll(&map, max_size);
+}
+
+pub fn mapBuilder(map: *std.ArrayHashMap(Coord, u8, std.array_hash_map.AutoContext(Coord), true), input: []const u8) !Coord {
     var max_size: Coord = .{ .x = 0, .y = 0 };
+    var current_pos: Coord = .{ .x = 0, .y = 0 };
     var lines = std.mem.splitScalar(u8, input, '\n');
     while (lines.next()) |line| {
         while (current_pos.x < line.len) {
             try map.put(current_pos, line[current_pos.x]);
+            current_pos.x += 1;
             if (current_pos.x > max_size.x) {
                 max_size.x = current_pos.x;
             }
-            current_pos.x += 1;
         }
-        if (current_pos.y > max_size.y) {
-            max_size.y = current_pos.y;
-        }
-        current_pos.y += 1;
         current_pos.x = 0;
+        current_pos.y += 1;
+        max_size.y = current_pos.y;
     }
+    return max_size;
+}
 
-    max_size.x += 1;
-    max_size.y += 1;
+pub fn removeRoll(map: *std.ArrayHashMap(Coord, u8, std.array_hash_map.AutoContext(Coord), true), max_size: Coord) !usize {
+    var valid_rolls: usize = 0;
 
     for (0..max_size.y) |y| {
         for (0..max_size.x) |x| {
             var roll_cnt: usize = 0;
             if ((map.get(.{ .x = x, .y = y }) orelse '?') != '.') {
-                const directions = [8][2]i32{
-                    .{ -1, 0 }, // left
-                    .{ 1, 0 }, // right
-                    .{ 0, -1 }, // up
-                    .{ 0, 1 }, // down
-                    .{ -1, -1 }, // top-left
-                    .{ 1, -1 }, // top-right
-                    .{ -1, 1 }, // bottom-left
-                    .{ 1, 1 }, // bottom-right
-                };
-
-                for (directions) |dir| {
+                for (DIRECTIONS) |dir| {
                     const nx = @as(i32, @intCast(x)) + dir[0];
                     const ny = @as(i32, @intCast(y)) + dir[1];
 
@@ -89,4 +93,18 @@ test "part 1" {
     const allocator = gpa.allocator();
 
     try std.testing.expectEqual(13, try part1(allocator, input));
+}
+
+test "map builder" {
+    const input = @embedFile("inputs/test_case.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var map = std.ArrayHashMap(Coord, u8, std.array_hash_map.AutoContext(Coord), true).init(allocator);
+    defer map.deinit();
+
+    const max_size = try mapBuilder(&map, input);
+    try std.testing.expectEqual(10, max_size.x);
+    try std.testing.expectEqual(10, max_size.y);
 }
