085e51f21f07a0e140ceb20ea468f0e30d42ec98
Austin <austin.a.guevara@gmail.com>
2025-12-05 11:17:01 -0800

    part 2 solution

--- Day_05/main.zig
+++ Day_05/main.zig
@@ -13,6 +13,7 @@ pub fn main() !void {
 
     const input = @embedFile("inputs/day05.txt");
     std.debug.print("Part 1 Answer: {d}\n", .{try part1(allocator, input)});
+    std.debug.print("Part 2 Answer: {d}\n", .{try part2(allocator, input)});
 
     const elapsed = timer.read();
     std.debug.print("Run Time: {d:.2}ms\n", .{@as(f64, @floatFromInt(elapsed)) / std.time.ns_per_ms});
@@ -48,6 +49,66 @@ pub fn part1(allocator: std.mem.Allocator, input: []const u8) !usize {
     return fresh_ingredients;
 }
 
+pub fn part2(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var fresh_ingredients: usize = 0;
+    var range: std.ArrayList(Range) = .empty;
+    defer range.deinit(allocator);
+
+    var lines = std.mem.splitScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        if (line.len == 0) break; // Handle change in sections
+
+        var bounds = std.mem.splitScalar(u8, line, '-');
+        const start_id = try std.fmt.parseInt(usize, bounds.next().?, 10);
+        const end_id = try std.fmt.parseInt(usize, bounds.next().?, 10);
+        const valid_range: Range = .{ .start = start_id, .end = end_id };
+        try range.append(allocator, valid_range);
+        std.debug.print("{s}\n", .{line});
+    }
+
+    var sorted = false;
+    while (!sorted) {
+        sorted = true;
+        var i: usize = 0;
+
+        while (i < range.items.len) {
+            var j: usize = i + 1;
+            var merged = false;
+
+            while (j < range.items.len) {
+                const existing = &range.items[i];
+                const check = &range.items[j];
+
+                if (existing.start <= check.end and existing.end >= check.start) {
+                    existing.start = @min(existing.start, check.start);
+                    existing.end = @max(existing.end, check.end);
+
+                    std.debug.print("{d}-{d} merged into {d}-{d}\n", .{ check.start, check.end, existing.start, existing.end });
+
+                    _ = range.orderedRemove(j);
+                    sorted = false;
+                    merged = true;
+                } else {
+                    j += 1;
+                }
+            }
+
+            if (merged) {
+                i = 0;
+            } else {
+                i += 1;
+            }
+        }
+    }
+
+    for (range.items) |value| {
+        std.debug.print("Counting Range: {d}-{d}\n", .{ value.start, value.end });
+        fresh_ingredients += (value.end - value.start + 1);
+    }
+
+    return fresh_ingredients;
+}
+
 test "part 1" {
     const input = @embedFile("inputs/test_case.txt");
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -55,4 +116,13 @@ test "part 1" {
     const allocator = gpa.allocator();
 
     try std.testing.expectEqual(3, try part1(allocator, input));
+}
+
+test "part 2" {
+    const input = @embedFile("inputs/test_case.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    try std.testing.expectEqual(14, try part2(allocator, input));
 }
