0b657202b9e9e42a9b60cae9faccbda56af5d68b
Austin <austin.a.guevara@gmail.com>
2025-12-10 11:37:37 -0800

    Day 10 initalization

---
+++ Day_10/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var timer = try std.time.Timer.start();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const input = @embedFile("inputs/day10.txt");
+    std.debug.print("Part 1 Answer: {d}\n", .{try part1(allocator, input)});
+
+    const elapsed = timer.read();
+    std.debug.print("Run Time: {d:.2}ms\n", .{@as(f64, @floatFromInt(elapsed)) / std.time.ns_per_ms});
+}
+
+fn part1(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        //TODO: Parse line
+    }
+
+    return 0;
+}
+
+test "part 1" {
+    const input = @embedFile("inputs/test_case.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    std.debug.print("\nRunning part 1 test...\n", .{});
+    try std.testing.expectEqual(7, try part1(allocator, input));
+}
