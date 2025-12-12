8a9b9ae7f4cf38bafc586ad8d931b91fdb903db4
Austin <austin.a.guevara@gmail.com>
2025-12-11 23:58:42 -0800

    parsing data

--- Day_12/main.zig
+++ Day_12/main.zig
@@ -1,5 +1,49 @@
 const std = @import("std");
 
+const State = enum {
+    PresentKey,
+    PresentShape,
+    TreeSize,
+};
+
+const Present = struct {
+    key: usize,
+    shape: [][]const u8,
+    area: usize,
+
+    fn init(key: usize, shape: [][]const u8) Present {
+        var area: usize = 0;
+        for (shape) |line| {
+            for (line) |c| {
+                if (c == '#') {
+                    area += 1;
+                }
+            }
+        }
+        return .{ .key = key, .shape = shape, .area = area };
+    }
+
+    fn deinit(self: *Present, allocator: std.mem.Allocator) void {
+        allocator.free(self.shape);
+    }
+};
+
+const Region = struct {
+    x: usize,
+    y: usize,
+    present_qty: []usize,
+    area: usize,
+
+    fn init(x: usize, y: usize, present_qty: []usize) Region {
+        const area = x * y;
+        return .{ .x = x, .y = y, .present_qty = present_qty, .area = area };
+    }
+
+    fn deinit(self: *Region, allocator: std.mem.Allocator) void {
+        allocator.free(self.present_qty);
+    }
+};
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
 
@@ -15,12 +59,59 @@ pub fn main() !void {
 }
 
 fn part1(allocator: std.mem.Allocator, input: []const u8) !usize {
-    _ = allocator;
+    var presents = std.AutoHashMap(usize, Present).init(allocator);
+    defer {
+        var iter = presents.valueIterator();
+        while (iter.next()) |present| {
+            present.deinit(allocator);
+        }
+        presents.deinit();
+    }
+    var regions: std.ArrayList(Region) = .empty;
+    defer {
+        for (regions.items) |*region| {
+            region.deinit(allocator);
+        }
+        regions.deinit(allocator);
+    }
+
+    var key: usize = 0;
+    var shape: std.ArrayList([]const u8) = .empty;
+    defer shape.deinit(allocator);
+    var curr_state = State.PresentShape;
+    var prev_state = curr_state;
 
     var lines = std.mem.splitScalar(u8, input, '\n');
     while (lines.next()) |line| {
-        _ = line;
-        //TODO: Handle each line
+        if (line.len == 0) continue;
+        if (std.mem.indexOf(u8, line, ":") == null) {
+            curr_state = State.PresentShape;
+            try shape.append(allocator, line);
+        } else if (std.mem.indexOf(u8, line, "x") != null) {
+            curr_state = State.TreeSize;
+            if (prev_state == State.PresentShape) {
+                try presents.put(key, Present.init(key, try shape.toOwnedSlice(allocator)));
+            }
+            var chunks = std.mem.splitScalar(u8, line, ':');
+            var grid = std.mem.splitScalar(u8, chunks.next().?, 'x');
+            const x = try std.fmt.parseInt(usize, grid.next().?, 10);
+            const y = try std.fmt.parseInt(usize, grid.next().?, 10);
+            var cnts = std.mem.splitScalar(u8, chunks.next().?, ' ');
+            var cnts_arr: std.ArrayList(usize) = .empty;
+            defer cnts_arr.deinit(allocator);
+            while (cnts.next()) |cnt| {
+                if (cnt.len == 0) continue;
+                try cnts_arr.append(allocator, try std.fmt.parseInt(usize, cnt, 10));
+            }
+            try regions.append(allocator, Region.init(x, y, try cnts_arr.toOwnedSlice(allocator)));
+        } else {
+            curr_state = State.PresentKey;
+            if (prev_state == State.PresentShape) {
+                try presents.put(key, Present.init(key, try shape.toOwnedSlice(allocator)));
+            }
+            key = try std.fmt.parseInt(usize, std.mem.trim(u8, line, ":"), 10);
+        }
+        prev_state = curr_state;
     }
 
     return 0;
