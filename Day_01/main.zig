a53a89fd01f2cd573f69dc78132676101eca7e98
Austin <austin.a.guevara@gmail.com>
2025-12-15 15:00:18 -0800

    combined parts

--- Day_01/main.zig
+++ Day_01/main.zig
@@ -2,54 +2,35 @@ const std = @import("std");
 
 const STARTING_LOCK_VALUE: i32 = 50;
 
+const Solution = struct {
+    part1: usize,
+    part2: usize,
+};
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
 
     const input = @embedFile("inputs/day01.txt");
-    std.debug.print("Part 1 Answer: {d}\n", .{try part1(input)});
-    std.debug.print("Part 2 Answer: {d}\n", .{try part2(input)});
+    const solution = try secretEntrance(input);
+    std.debug.print("Part 1 Answer: {d}\n", .{solution.part1});
+    std.debug.print("Part 2 Answer: {d}\n", .{solution.part2});
 
     const elapsed = timer.read();
     std.debug.print("Run Time: {d:.2}ms\n", .{@as(f64, @floatFromInt(elapsed)) / std.time.ns_per_ms});
 }
 
-fn part1(input: []const u8) !i32 {
-    var code: i32 = 0;
+fn secretEntrance(input: []const u8) !Solution {
     var lock_value: i32 = STARTING_LOCK_VALUE;
-    var lines = std.mem.splitScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        const ticks = @mod(try std.fmt.parseInt(i32, line[1..], 10), 100);
-        switch (line[0]) {
-            'R' => lock_value += ticks,
-            'L' => lock_value -= ticks,
-            else => return error.InvalidDirection,
-        }
+    var part1: usize = 0;
+    var part2: usize = 0;
 
-        // Handle wrap-around
-        if (lock_value < 0) {
-            lock_value += 100;
-        } else if (lock_value >= 100) {
-            lock_value -= 100;
-        }
-
-        // Check for code match
-        if (lock_value == 0) {
-            code += 1;
-        }
-    }
-    return code;
-}
-
-fn part2(input: []const u8) !usize {
-    var code: usize = 0;
-    var lock_value: i32 = STARTING_LOCK_VALUE;
     var lines = std.mem.splitScalar(u8, input, '\n');
     while (lines.next()) |line| {
         const ticks = try std.fmt.parseInt(usize, line[1..], 10);
 
         for (0..ticks) |_| {
             if (lock_value == 0) {
-                code += 1;
+                part2 += 1;
             }
             switch (line[0]) {
                 'R' => lock_value += 1,
@@ -63,16 +44,22 @@ fn part2(input: []const u8) !usize {
                 lock_value -= 100;
             }
         }
+        if (lock_value == 0) {
+            part1 += 1;
+        }
     }
-    return code;
+
+    return .{ .part1 = part1, .part2 = part2 };
 }
 
 test "part 1" {
     const input = @embedFile("inputs/test_case.txt");
-    try std.testing.expectEqual(3, try part1(input));
+    const part1 = (try secretEntrance(input)).part1;
+    try std.testing.expectEqual(3, part1);
 }
 
 test "part 2" {
     const input = @embedFile("inputs/test_case.txt");
-    try std.testing.expectEqual(6, try part2(input));
+    const part2 = (try secretEntrance(input)).part2;
+    try std.testing.expectEqual(6, part2);
 }
